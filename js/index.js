2476165b010ddb99beb3107811d90e2e4cf69878
Andrew Sitzes <andrewsitzes@Andrews-MacBook-Pro.local>
2020-03-11 16:33:34 -0700

    Mvp

--- js/index.js
+++ js/index.js
@@ -2,11 +2,11 @@
 
 // Your code goes here
 
-document.querySelector('a').addEventListener('click', clickEventHandler)
-function clickEventHandler(event) {
-  console.log(event.type + '!!!!!!!!!!!!!!!!!!!!!!!!!!!')
-  console.log(event.target);
-}
+// document.querySelector('a').addEventListener('click', clickEventHandler)
+// function clickEventHandler(event) {
+//   console.log(event.type + '!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+//   console.log(event.target);
+// }
   
 //   document.querySelector('nav').addEventListener('click', clickEventHandler)
 //   document.querySelector('header').addEventListener('click', clickEventHandler)
@@ -14,9 +14,6 @@ function clickEventHandler(event) {
 //   document.addEventListener('click', clickEventHandler)
 //   window.addEventListener('click', clickEventHandler)
 //Declarations
-
-
-
 
 
 
@@ -49,10 +46,11 @@ pageImg.addEventListener("mouseleave", (event) => {
 });
 
 
-const h2Color = document.querySelector('h2');
-h2Color.addEventListener('click',(event) => {
+const h2Color = document.querySelectorAll('h2');
+Array.from(h2Color).forEach((apple) => {
+apple.addEventListener('click',(event) => {
 	console.log(event.target.style.color = 'red');
-	
+	});
 });
 
 const buttons = document.querySelectorAll(".btn");
@@ -60,4 +58,16 @@ Array.from(buttons).forEach((banana) => {
 banana.addEventListener('click', (event) => {
 	event.target.style.color = 'orange';
 	});
+});
+
+const imgs = document.querySelectorAll('img')
+Array.from(imgs).forEach((pear) => {
+pear.addEventListener('click', (event) => {
+	event.target.style.filter = "blur(8px)";
+	});
+});
+
+const foot = document.querySelector('footer p');
+foot.addEventListener('click', (event) => {
+	event.target.style.color = 'pink';
 });
